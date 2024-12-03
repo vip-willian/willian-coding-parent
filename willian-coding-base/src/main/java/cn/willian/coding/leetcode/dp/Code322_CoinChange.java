2f2186ddb48451d76684a15632031afad0c7876b
willian <willian.wyann@gmail.com>
2024-12-03 22:27:31 +0800

    动态规划-换硬币

--- willian-coding-base/src/main/java/cn/willian/coding/leetcode/dp/Code322_CoinChange.java
+++ willian-coding-base/src/main/java/cn/willian/coding/leetcode/dp/Code322_CoinChange.java
@@ -19,15 +19,18 @@ public class Code322_CoinChange {
 
     public static int coinChange(int[] coins, int amount) {
 
+        // 从小往大的金额去推
+        // 遍历全部硬币数组，去获取最小使用的零钱数
         int max = amount + 1;
-        int[] dp = new int[amount + 1];
+        int[] dp = new int[max];
         Arrays.fill(dp, max);
         dp[0] = 0;
-        for (int i = 1; i <= amount; i++) {
+        for (int rest = 1; rest <= amount; rest++) {
             // 最小硬币数量 等于 f(1,10)、f(2,9),f(5,6) 的 最小值 + 当前自身已选择了一枚硬币
-            for (int j = 0; j < coins.length; j++) {
-                if (coins[j] <= i) {
-                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+            for (int coin : coins) {
+                if (coin <= rest) {
+                    // 之前找到最少的硬币数 和 现在选择的一个硬币数 取最小值
+                    dp[rest] = Math.min(dp[rest], dp[rest - coin] + 1);
                 }
             }
         }
